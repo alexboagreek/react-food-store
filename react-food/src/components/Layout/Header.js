d1e3ccb0feb7049e16cf9ce0cff20c7cad7d3811
Alex <olivesargreek@gmail.com>
2022-03-03 15:41:57 +0300

    create: HeaderCartButton

--- react-food/src/components/Layout/Header.js
+++ react-food/src/components/Layout/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import sushi from '../../assets/sushi.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
 
 const Header = (props) => {
     return (
         <React.Fragment>
             <header className={ styles.header }>
                 <h1>React Sushi Bar</h1>
-                <button>Basket</button>
+                <HeaderCartButton />
             </header>
-            <div className={ styles['main-image']}>
+            <div className={ styles['main-image'] }>
                 <img src={ sushi } alt="logo" />
             </div>
             
