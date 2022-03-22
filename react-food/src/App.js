c091ee262fb93e5436b09c252c51bb7b6f10d071
Alex <olivesargreek@gmail.com>
2022-03-22 13:04:56 +0300

    create: CartContextProvider

--- react-food/src/App.js
+++ react-food/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Header from "./components/Layout/Header";
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartContextProvider from './store/CartContextProvider';
 
 function App() {
 
@@ -17,13 +18,13 @@ function App() {
 
 
   return (
-    <React.Fragment>
+    <CartContextProvider>
       { cartIsVisible && <Cart onHideCart={ hideCartHandler } /> }
       <Header onShowCart={ showCartHandler }/>
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartContextProvider>
   );
 }
 
